7c9847a8c9dca308a08ef60bf443adc244960729
gabinho69 <gabin.odin69@gmail.com>
2022-01-21 16:10:01 +0100

    update mon_espace

--- mon_espace.php
+++ mon_espace.php
@@ -1,9 +1,70 @@
-<?php 
+<!--- Accessible que si connecté ---->
 
-session_start();
+<!doctype html>
 
-if(!isset($_SESSION['user'])){
-	header('Location:login.php');
-	die();
-}
-?>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Acceuil</title>
+
+<link rel="stylesheet" href="./css/style.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+</head>
+  <body>
+  <nav>
+    <div class="wrapper">
+      <div class="logo"><a href="index.php">Open Parc Lyon Billeterie</a></div>
+      <input type="radio" name="slider" id="menu-btn">
+      <input type="radio" name="slider" id="close-btn">
+      <ul class="nav-links">
+        <label for="close-btn" class="btn close-btn"><i class="fas fa-times"></i></label>
+		
+        <li><a href="index.php">Accueil</a></li>
+		
+        <li><a href="liste-matchs-simple.php">Simple</a></li>
+	   
+        <li><a href="liste-matchs-double.php">Double</a></li>
+
+        <li class="hidden-space">hola<li>
+
+	   <li>
+          <a href="mon_espace.php" class="desktop-item">Espace Perso</a>
+          <input type="checkbox" id="showDrop">
+          <label for="showDrop" class="mobile-item">Espace Perso</label>
+          <ul class="drop-menu">
+            <li><a href="mon_espace.php">Mon espace</a></li>
+
+            <li><a href="deconnexion.php">Deconnexion</a></li>
+          </ul>
+        </li>
+      </ul>
+      <label for="menu-btn" class="btn menu-btn"><i class="fas fa-bars"></i></label>
+    </div>
+  </nav>
+
+
+  <footer class="footer">
+    <div class="footer-top">
+        <div class="footer-about">
+            <h3> A PROPOS </h3>
+            <p> Le tournoi de tennis de Lyon est un tournoi international de tennis masculin et féminin des circuits professionnels de l'ATP World Tour et de la WTA. Organisé par Sport Plus Conseil, le tournoi regroupe 28 joueurs et joueuse en simple et 16 équipe en binôme.
+            </p>
+        </div>
+
+        <div class="footer-links">
+            <h3> LIENS UTILES </h3>
+            <ul>
+                <li><a href="https://openparc.com/" target="_blank"> Site web </a></li>
+                <li><a href="https://twitter.com/OpenParcARA" target="_blank"> Twitter </a></li>
+                <li><a href="https://www.facebook.com/openparcauvergnerhonealpes/" target="_blank"> Facebook </a></li>
+            <ul>
+        </div>
+    </div>
+
+    <div class="footer-bottom">
+    <p>Copyright © 2021 IUT info Lyon 1 </p>
+    </div>
+</footer>
+
+</body>
+</html>
