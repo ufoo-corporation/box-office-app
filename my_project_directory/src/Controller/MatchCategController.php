9178750b7f42653fc98470df0ea93c00480a782d
Guimleh <guillaume.fiquemo@gmail.com>
2022-01-23 15:54:37 +0100

    Implémentation BD Day

--- my_project_directory/src/Controller/MatchCategController.php
+++ my_project_directory/src/Controller/MatchCategController.php
@@ -5,14 +5,24 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Day;
+use App\Repository\DayRepository;
 
 class MatchCategController extends AbstractController
 {
-    #[Route('/match-categ', name: 'match_categ')]
-    public function index(): Response
+    #[Route('/match-categ/{id}', name: 'match_categ')]
+    public function index(Day $day): Response
     {
+        switch($date){
+
+        }
+
         return $this->render('match_categ/index.html.twig', [
-            'controller_name' => 'MatchCategController',
+            'loges' => $day->getPlacesLoges(),
+            'cat1' => $day->getPlacesCat1(),
+            'cat2' => $day->getPlacesCat2(),
+            'date' => $day->getDate(),
+
         ]);
     }
 }
